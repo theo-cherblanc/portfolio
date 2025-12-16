36510147cae246c18287769901806b58e2e7560b
fumikage <theocherblanc@pc-282.home>
2025-12-16 11:41:48 +0100

    feat: add next-intl for init traduction

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,31 @@
+import { Locale } from "next-intl";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+import Image from "next/image";
+
+export default async function Home({ params }: PageProps<"/[locale]">) {
+    const { locale } = await params;
+
+    // Enable static rendering
+    setRequestLocale(locale as Locale);
+    const t = await getTranslations("Home");
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+                <Image
+                    className="dark:invert"
+                    src="/next.svg"
+                    alt="Next.js logo"
+                    width={100}
+                    height={20}
+                    priority
+                />
+                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                        {t("title")}
+                    </h1>
+                </div>
+            </main>
+        </div>
+    );
+}
